c5cf24fdf3524cd0eedb3b2f1c6fb71f4f8d7a85
amarmahdin <120645744+amarmahdin@users.noreply.github.com>
2026-09-24 21:37:46 +0700

    add svg

--- src/components/Ornament.vue
+++ src/components/Ornament.vue
@@ -3,3 +3,13 @@ defineProps({
   className: { type: String, default: 'text-gold' },
 })
 </script>
+
+<template>
+<svg
+    :class="className"
+    viewBox="0 0 120 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+</template>
