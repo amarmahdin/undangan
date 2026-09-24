502506ddad14eff1a52b1f4a3cc2e9578f250e43
amarmahdin <120645744+amarmahdin@users.noreply.github.com>
2026-09-24 21:43:26 +0700

    add svg

--- src/components/FloralCorner.vue
+++ src/components/FloralCorner.vue
@@ -5,4 +5,14 @@ defineProps({
 </script>
 
 <template>
+<svg
+    viewBox="0 0 200 240"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    class="pointer-events-none h-auto w-full"
+    :class="side === 'right' ? '-scale-x-100' : ''"
+  >
+  
+  </svg>
 </template>
